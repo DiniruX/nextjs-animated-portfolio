daf99e9743f8195a22a12a310710a61cafbc24a6
Diniru Sandipa <it20041120@my.sliit.lk>
2024-03-03 01:35:59 +0530

    changes

--- loader.js
+++ loader.js
@@ -2,5 +2,6 @@
  
 export default function myImageLoader({ src, width, quality }) {
     if(src.startsWith('https://images.pexels.com'))return src
+    if(src.startsWith('https://res.cloudinary.com'))return src
   return `https://dinirudev.com/${src}?w=${width}&q=${quality || 75}`
 }
